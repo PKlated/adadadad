89f8b5db21158976488971bd0663daacf99cc64f
PKlated <106903353+PKlated@users.noreply.github.com>
2023-11-09 22:32:47 +0700

    Add files via upload

--- week3.8.js
+++ week3.8.js
@@ -3,15 +3,22 @@ let re = document.getElementById("result");
 function computeData(){
     let bottle =parseFloat(b1.value);
     if (bottle>12){
-        let x=bottle/12
-        if (x-Math.round(x)=0){
-            bottle-(Math.round)
+        let x = bottle/12
+        if (x - Math.ceil(x) != 0){
+            let y = bottle-(Math.floor(x)*12) 
+            ans = (Math.floor(x)*100)+(y*10.7)
         }
+        else {
+            ans = x*100
+        }
+    }
+    else {
+        ans = bottle*10.7  
     }
     re.innerHTML = "ans = "+ans;
 }
 
 function resetData(){
-    n1.value="";
+    b1.value="";
     re.innerHTML="";
 }
