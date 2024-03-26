d0e6ff9e30a68b50adae48c25cdc8168285743fd
PKlated <106903353+PKlated@users.noreply.github.com>
2024-03-26 13:12:11 +0700

    Add files via upload

--- 105/4/stack.js
+++ 105/4/stack.js
@@ -37,7 +37,8 @@ let e = document.getElementById("exe")
 let r = document.getElementById("result")
 function displayResult(){
     let result = infixToPostfix(e.value);
-    r.innerHTML = "result = " +result;
+    let ans = evaluatePostfix(result);
+    r.innerHTML = "Postfix = " +result + "</br>"+ " Answer = " + ans;
 }
 // let result = infixToPostfix("((a+b)*c/d+e^f)/g")
 // console.log("result = ",result)
@@ -100,6 +101,63 @@ function checkPriority(c){
         return 0;
     }
 }
+
+function evaluatePostfix(postfix) {
+    const stack = new Stack();
+
+    for (let i = 0; i < postfix.length; i++) {
+        let ch = postfix.charAt(i);
+
+        if (!isNaN(ch)) {
+            stack.push(parseInt(ch));
+        } else if (isOperator(ch)) {
+            let operand2 = stack.pop();
+            let operand1 = stack.pop();
+            let res;
+            
+            if (operand2 === 0 && ch === '/') {
+                res = "Please input again"; // Handle division by zero
+            } else {    
+                res = applyOperator(operand1, operand2, ch);
+            }
+
+            stack.push(res);
+        } else {
+            return "Please input a number only"; // Return error message for invalid input characters
+        }
+    }
+
+    if (stack.isEmpty()) {
+        return "Please input a number only"; // Return error message if the stack is empty (input was incomplete)
+    }
+
+    return stack.pop();
+}
+
+
+
+function isOperator(ch) {
+    return ch === '+' || ch === '-' || ch === '*' || ch === '/';
+}
+
+function applyOperator(operand1, operand2, operator) {
+    if (operator === '+') {
+        return operand1 + operand2;
+    } else if (operator === '-') {
+        return operand1 - operand2;
+    } else if (operator === '*') {
+        return operand1 * operand2;
+    } else if (operator === '/') {
+        if (operand2 !== 0) {
+            return operand1 / operand2;
+        } else {
+            return "Please input again"; // Return the error message as a string
+        }
+    } else {
+        throw new Error("Invalid operator: " + operator); // Throw an Error object for invalid operators
+    }
+}
+
 // let mystack = new Stack();
 // console.log(mystack);
 // console.log(mystack.isEmty());
